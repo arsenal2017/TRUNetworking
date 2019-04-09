211cd4608f1a494e51bbf5bc3c8f9de5c1570a12
arsenal2017 <1274163421@qq.com>
2019-04-09 17:09:20 +0800

    init project

---
+++ TRUNetworking/Classes/LZNetworking.h
@@ -0,0 +1,18 @@
+//
+//  LZNetworking.h
+//  TRUNetworking_Example
+//
+//  Created by 赵子远 on 2019/4/9.
+//  Copyright © 2019 arsenal2017. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZNetworking : NSObject
+-(void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/LZNetworking.m
@@ -0,0 +1,18 @@
+//
+//  LZNetworking.m
+//  TRUNetworking_Example
+//
+//  Created by 赵子远 on 2019/4/9.
+//  Copyright © 2019 arsenal2017. All rights reserved.
+//
+
+#import "LZNetworking.h"
+
+
+@implementation LZNetworking
+-(void)test
+{
+    NSLog(@"TEST");
+}
+
+@end
